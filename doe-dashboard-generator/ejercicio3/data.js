e28e885f390df95301281542ce82069fa01d36d6
Brenda-hi <brenda.hidalgo.d@uni.pe>
2026-06-04 17:04:41 -0500

    Add files via upload

---
+++ doe-dashboard-generator/ejercicio3/data.js
@@ -0,0 +1,27 @@
+document.getElementById("tipoEjercicio")
+.innerText = "CUADRADO LATINO";
+
+document.getElementById("tituloEjercicio")
+.innerText = "Cuadrado Latino 4×4";
+
+document.getElementById("kpiTipo")
+.innerText = "Latino";
+
+document.getElementById("estudiante")
+.innerText = "Brenda Hidalgo";
+
+document.getElementById("curso")
+.innerText = "Diseño de Experimentos";
+
+document.getElementById("universidad")
+.innerText = "IDAT";
+
+document.getElementById("fecha")
+.innerText = "2026";
+
+document.getElementById("conclusiones")
+.innerText =
+`El análisis permitió identificar diferencias
+significativas entre tratamientos y validar
+el comportamiento experimental mediante
+ANOVA y pruebas complementarias.`;
